47a6e8d0e42ebb36b645c6810511650ff15e810e
THRIVARSHITHA <24wh1a05x8@bvrithyderabad.edu.in>
2025-02-27 17:58:28 +0530

    Created using Colab

---
+++ sample code.py
@@ -0,0 +1,58 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyMdE/ocpUldFhJ/89e0nknw",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/THRIVARSHITHA/code/blob/main/sample%20code.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": 4,
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "NBm7LshNX6Lj",
+        "outputId": "fdc0851d-2459-4620-e8d7-405113205590"
+      },
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "enter the number4639483549\n",
+            "odd\n"
+          ]
+        }
+      ],
+      "source": [
+        "n=int(input(\"enter the number\"))\n",
+        "if n%2==0 :\n",
+        "  print(\"even\")\n",
+        "else:\n",
+        "  print(\"odd\")"
+      ]
+    }
+  ]
+}
